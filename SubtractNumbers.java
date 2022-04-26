add6705860091cf4618d5e12dd7e70bb627a07e4
adi080700 <adityacdingankar@gmail.com>
2022-04-26 22:49:58 +0530

    subtract

---
+++ SubtractNumbers.java
@@ -0,0 +1,10 @@
+class SubtractNumbers {
+
+   public static void main(String[] args) {
+        
+      int num1 = 15, num2 = 5, sum;
+      diff = num1 - num2;
+
+      System.out.println("Difference between these numbers: "+diff);
+   }
+}
