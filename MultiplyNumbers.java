e3322536dc9ee6183af5159b8259f2660c2b76ca
adi080700 <adityacdingankar@gmail.com>
2022-04-26 23:01:20 +0530

    mul

---
+++ MultiplyNumbers.java
@@ -0,0 +1,10 @@
+class MultiplyNumbers {
+
+   public static void main(String[] args) {
+        
+      int num1 = 15, num2 = 5, mul;
+      mul = num1*num2;
+
+      System.out.println("Multiplication of these numbers: "+mul);
+   }
+}
