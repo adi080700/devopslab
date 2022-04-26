62465ee65a25a5502cb084e8cd49d287f1b50fa3
adi080700 <adityacdingankar@gmail.com>
2022-04-26 22:53:13 +0530

    sub

--- SubtractNumbers.java
+++ SubtractNumbers.java
@@ -2,7 +2,7 @@ class SubtractNumbers {
 
    public static void main(String[] args) {
         
-      int num1 = 15, num2 = 5, sum;
+      int num1 = 15, num2 = 5, diff;
       diff = num1 - num2;
 
       System.out.println("Difference between these numbers: "+diff);
